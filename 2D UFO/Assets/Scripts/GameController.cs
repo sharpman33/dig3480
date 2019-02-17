42301d89f1d036b11be84fc17ace2bab7571e9ba
Tony Chen <tonychen820@gmail.com>
2019-02-17 14:19:55 -0500

    2D UFO Game

---
+++ 2D UFO/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+            Debug.Log("Exit Game");
+        }
+    }
+}
